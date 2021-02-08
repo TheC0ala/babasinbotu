03f7860d24e19433549a410f2dc6948e50fc092d
TheC0ala <76583019+TheC0ala@users.noreply.github.com>
2021-02-08 22:41:31 +0400

    Create weather.py

--- tg_bot/modules/weather.py
+++ tg_bot/modules/weather.py
@@ -1,7 +1,7 @@
-import pyowm
 from pyowm import timeutils, exceptions
 from telegram import Message, Chat, Update, Bot
 from telegram.ext import run_async
+import pyowm
 
 from tg_bot import dispatcher, updater, API_WEATHER
 from tg_bot.modules.disable import DisableAbleCommandHandler
